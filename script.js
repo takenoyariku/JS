686f1b95d45db262232deb187d26b48f698a0bc3
Takenoya Riku <91358267+takenoyariku@users.noreply.github.com>
2022-05-26 14:41:13 +0900

    インラインスタイル編

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 window.addEventListener('mouseout', function(){
   const pullDownButton = document.getElementById("lists")
-  console.log(pullDownButton)
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:blue;")
+  })
+  pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style", "background-color:red;")
+  })
+  pullDownButton.addEventListener('click', function(){
+    pullDownButton.setAttribute("style", "background-color:green;")
+  })
 })
