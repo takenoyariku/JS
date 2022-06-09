718e98367160885b26a7c73a006d7a9b7d8f32f2
Takenoya Riku <91358267+takenoyariku@users.noreply.github.com>
2022-06-09 18:06:41 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
-window.addEventListener('mouseout', function(){
+window.addEventListener('mouseout', function() {
   const pullDownButton = document.getElementById("lists")
-  pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:blue;")
+  const pullDownParents = document.getElementById("pull-down")
+  pullDownButton.addEventListener('mouseover', function() {
+    this.setAttribute("style", "background-color:blue;")
   })
-  pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:red;")
+  pullDownButton.addEventListener('mouseout', function() {
+    this.removeAttribute("style", "background-color:red;")
   })
-  pullDownButton.addEventListener('click', function(){
-    pullDownButton.setAttribute("style", "background-color:green;")
+  pullDownButton.addEventListener('click', function() {
+    this.setAttribute("style", "background-color:green;")
+    if(pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+      console.log("非表示")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+      console.log("表示")
+    }
   })
 })
